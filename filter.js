99b751c12fcf79e3cb29eaca7d0d50758ef4b466
Oliver Sharp <olliesharp5@gmail.com>
2024-01-09 15:57:42 +0000

    Added lesson content

--- filter.js
+++ filter.js
@@ -21,6 +21,12 @@ const people = [
   },
 ];
 
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(person => person.name === "Paul")[0];
+console.log(paul);
+
 
 // Complex Filtering
 const students = [
@@ -55,3 +61,10 @@ const students = [
     ]
   },
 ];
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
+
+
